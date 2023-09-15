68eb9199fa071cea847347dfddbcb11be908ebac
abdullahdgrey8 <abdullahkhalid204606@gmail.com>
2023-09-15 15:45:17 +0500

    section of SPA completed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,25 @@
 import styles from "./Map.module.css";
+import { useSearchParams, useNavigate } from "react-router-dom";
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>MAP</h1>
+      <h1>
+        Position {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 90 });
+        }}
+      >
+        Change Position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
