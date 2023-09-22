9a73ab2a913ab7855eb184c5b7bd18bf1ac4513d
abdullahdgrey8 <abdullahkhalid204606@gmail.com>
2023-09-22 12:10:58 +0500

    implemented Lazy loading

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,6 @@
 import {
   createContext,
+  useCallback,
   useContext,
   useEffect,
   useReducer,
@@ -82,21 +83,25 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: "loading" });
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
 
-    try {
-      const res = await fetch(`${URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payLoad: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payLoad: "There was an error loading city...",
-      });
-    }
-  }
+      try {
+        const res = await fetch(`${URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payLoad: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payLoad: "There was an error loading city...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
+
   async function createCity(newCity) {
     try {
       dispatch({ type: "loading" });
